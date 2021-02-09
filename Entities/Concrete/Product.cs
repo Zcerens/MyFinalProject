2e8eac91c03d16af89b09469a571f1f8d31b955e
Ceren <Zcerens@users.noreply.github.com>
2021-02-10 02:29:03 +0300

    Add project files.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //internal class --> sadece bulundugu katman erişir.(public zıttı)
+    public class Product : IEntity
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; } 
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+
+    }
+}
